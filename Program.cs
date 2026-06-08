3936c7e0c2d42d3d9dd72cf45120f9f562eb403a
cuscapi <uscapiman@gmail.com>
2026-06-07 19:34:07 -0500

    Implementacion de menu de ejemplo

--- Program.cs
+++ Program.cs
@@ -5,10 +5,15 @@ namespace MenuBonito
     class Program
     {
         static void Main(string[] args)
-        {
-            SumaqMenu menu = new SumaqMenu("MIS HERMANOS LOS TONTOS");
-            menu.AgregarOpcion("Glados Huarraca ayqui");
-            menu.AgregarOpcion("hola cristofer uscaoi baez");
+        { 
+            // Instanciamos objeto de la clase Sumaq Menu
+            SumaqMenu menu = new SumaqMenu("MI MENU");
+            menu.AgregarOpcion("Opcion 1");
+            menu.AgregarOpcion("Opcion 2");
+            menu.AgregarOpcion("Opcion 3");
+            menu.AgregarOpcion("Opcion 4");
+            menu.AgregarOpcion("Opcion 5");
+            menu.AgregarOpcion("Opcion 6");
             menu.MostrarMenu1();
         }
     }
